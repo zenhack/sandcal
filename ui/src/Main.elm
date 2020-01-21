ec8ec55370ed8fd26772c0f730d112eb6ed5ac5d
Ian Denhardt <ian@zenhack.net>
2020-01-21 13:25:16 -0500

    Some work on turning this into an SPA.

--- ui/src/Main.elm
+++ ui/src/Main.elm
@@ -1,32 +1,65 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Http
 import SandCal.Api as Api
 import SandCal.Types as Types
 import Time
+import Url
 
 
 type Model
     = Model
-        { events : Maybe (Result Http.Error (List Types.Event))
+        { page : Page
+        , navKey : Nav.Key
         }
 
 
+type alias EventsResult =
+    Result Http.Error (List Types.Event)
+
+
+type Page
+    = EventsPage { events : Maybe EventsResult }
+
+
 type Msg
+    = ClickedLink Browser.UrlRequest
+    | UrlChange Url.Url
+    | PageMsg PageMsg
+
+
+type PageMsg
+    = EventsPageMsg EventsPageMsg
+
+
+type EventsPageMsg
     = AllEventsResult (Result Http.Error (List Types.Event))
 
 
-init : {} -> ( Model, Cmd Msg )
-init _ =
-    ( Model { events = Nothing }
-    , Api.allEvents AllEventsResult
+init : {} -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ _ navKey =
+    ( Model
+        { navKey = navKey
+        , page = EventsPage { events = Nothing }
+        }
+    , Api.allEvents (AllEventsResult >> EventsPageMsg >> PageMsg)
     )
 
 
-view : Model -> Html msg
-view (Model { events }) =
+view : Model -> Browser.Document msg
+view (Model { page }) =
+    case page of
+        EventsPage { events } ->
+            { title = "SandCal"
+            , body = [ viewEvents events ]
+            }
+
+
+viewEvents : Maybe EventsResult -> Html msg
+viewEvents events =
     case events of
         Nothing ->
             text "Loading..."
@@ -45,11 +78,51 @@ view (Model { events }) =
                 )
 
 
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest =
+    ClickedLink
+
+
+onUrlChange : Url.Url -> Msg
+onUrlChange =
+    UrlChange
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
-update (AllEventsResult res) (Model m) =
-    ( Model { m | events = Just res }
-    , Cmd.none
-    )
+update msg (Model m) =
+    case msg of
+        PageMsg pageMsg ->
+            let
+                ( newPage, cmd ) =
+                    updatePage pageMsg m.page
+            in
+            ( Model { m | page = newPage }
+            , Cmd.map PageMsg cmd
+            )
+
+        ClickedLink (Browser.Internal url) ->
+            ( Model m
+            , Nav.pushUrl m.navKey (Url.toString url)
+            )
+
+        ClickedLink (Browser.External url) ->
+            ( Model m
+            , Nav.load url
+            )
+
+        UrlChange _ ->
+            ( Model m
+            , Cmd.none
+            )
+
+
+updatePage : PageMsg -> Page -> ( Page, Cmd PageMsg )
+updatePage (EventsPageMsg msg) (EventsPage p) =
+    case msg of
+        AllEventsResult res ->
+            ( EventsPage { p | events = Just res }
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
@@ -58,9 +131,11 @@ subscriptions _ =
 
 
 main =
-    Browser.element
+    Browser.application
         { init = init
         , view = view
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
         , update = update
         , subscriptions = subscriptions
         }
