48810c9bf9a87bd2f5ec0efbf78a09e269932098
Ian Denhardt <ian@zenhack.net>
2020-01-20 22:40:31 -0500

    More work on submitting events

--- src/Forms.hs
+++ src/Forms.hs
@@ -19,29 +19,48 @@ data HtmlDate
     = HtmlDate Int Int Int
     deriving(Show, Read, Eq)
 
+data HtmlTime
+    = HtmlTime Int Int
+    deriving(Show, Read, Eq)
+
+parsecParsable :: P.Parsec LT.Text () a -> LT.Text -> Either LT.Text a
+parsecParsable p txt =
+    case P.parse p "" txt of
+        Left e  -> Left (LT.pack $ show e)
+        Right v -> Right v
+
+nDigits :: Int -> P.Parsec LT.Text () Int
+nDigits n =
+    Prelude.read <$> P.count n P.digit
+
 instance Parsable HtmlDate where
-    parseParam txt =
-        case P.parse dateParser "" txt of
-            Left e  -> Left (LT.pack $ show e)
-            Right v -> Right v
+    parseParam = parsecParsable $ do
+        year <- nDigits 4
+        P.char '-'
+        month <- nDigits 2
+        P.char '-'
+        day <- nDigits 2
+        pure $ HtmlDate year month day
+
+instance Parsable HtmlTime where
+    parseParam = parsecParsable $ do
+        hour <- nDigits 2
+        P.char ':'
+        minute <- nDigits 2
+        pure $ HtmlTime hour minute
 
 data NewEvent = NewEvent
-    { neStart   :: HtmlDate
-    , neSummary :: LT.Text
+    { neStartDate :: HtmlDate
+    , neStartTime :: HtmlTime
+    , neEndTime   :: HtmlTime
+    , neSummary   :: LT.Text
     }
     deriving(Show, Read, Eq)
 
 instance IsForm NewEvent where
     parseForm = do
-        neStart <- param "startDate"
+        neStartDate <- param "startDate"
+        neStartTime <- param "startTime"
+        neEndTime <- param "endTime"
         neSummary <- param "summary"
         pure NewEvent{..}
-
-
-dateParser = do
-    year <- Prelude.read <$> P.count 4 P.digit
-    P.char '-'
-    month <- Prelude.read <$> P.count 2 P.digit
-    P.char '-'
-    day <- Prelude.read <$> P.count 2 P.digit
-    pure $ HtmlDate year month day
