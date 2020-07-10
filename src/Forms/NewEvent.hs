462bea09349b9b36bdec82f68f5e49a6ae76060d
Ian Denhardt <ian@zenhack.net>
2020-07-10 03:17:39 -0400

    Move VEvent construction into the NewEvent module.

--- src/Forms/NewEvent.hs
+++ src/Forms/NewEvent.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 module Forms.NewEvent
     ( NewEvent(..)
     , NewEventTime(..)
     , getForm
+    , toVEvent
     ) where
 
 import Zhp
@@ -11,10 +13,17 @@ import Web.Scotty
 
 import Forms.Common
 
-import qualified Data.Map.Strict     as M
-import qualified Data.Text.Lazy      as LT
-import qualified Data.Time           as Time
-import qualified Data.Time.Zones.All as Tz
+import Data.Default             (def)
+import Data.Text.Encoding.Error (lenientDecode)
+
+import qualified Data.ByteString.Lazy    as LBS
+import qualified Data.Map.Strict         as M
+import qualified Data.Set                as S
+import qualified Data.Text.Lazy          as LT
+import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.Time               as Time
+import qualified Data.Time.Zones.All     as Tz
+import qualified Data.UUID               as UUID
 
 import qualified DateParsers as DP
 import qualified ICal
@@ -80,3 +89,128 @@ freqNames =
     ]
     & map (\freq -> (show freq, freq))
     & M.fromList
+
+toVEvent :: Time.UTCTime -> UUID.UUID -> NewEvent -> ICal.VEvent
+toVEvent utcNow uuid Forms.NewEvent.NewEvent{ summary, description, date, time, repeats } =
+    let (start, end) = case time of
+            Forms.NewEvent.AllDay ->
+                ( ICal.DTStartDate
+                    { ICal.dtStartOther = def
+                    , ICal.dtStartDateValue = ICal.Date date
+                    }
+                , ICal.DTEndDate
+                    { ICal.dtEndDateValue = ICal.Date date -- TODO/FIXME: should this be exclusive?
+                    , ICal.dtEndOther = def
+                    }
+                )
+            Forms.NewEvent.StartEnd { startTime, endTime, timeZone } ->
+                let floatingStart = Time.LocalTime
+                        { Time.localDay = date
+                        , Time.localTimeOfDay = startTime
+                        }
+                    floatingEnd = floatingStart
+                        { Time.localTimeOfDay = endTime
+                        }
+                    dtStart = ICal.ZonedDateTime
+                        { ICal.dateTimeFloating = floatingStart
+                        , ICal.dateTimeZone = encodeTZLabel timeZone
+                        }
+                    dtEnd = dtStart { ICal.dateTimeFloating = floatingEnd }
+                in
+                ( ICal.DTStartDateTime
+                    { ICal.dtStartOther = def
+                    , ICal.dtStartDateTimeValue = dtStart
+                    }
+                , ICal.DTEndDateTime
+                    { ICal.dtEndDateTimeValue = dtEnd
+                    , ICal.dtEndOther = def
+                    }
+                )
+    in
+    ICal.VEvent
+        { ICal.veUID = ICal.UID
+            { ICal.uidValue = LT.fromStrict $ UUID.toText uuid
+            , ICal.uidOther = def
+            }
+        , ICal.veDTStamp = ICal.DTStamp
+            { ICal.dtStampValue = utcNow
+            , ICal.dtStampOther = def
+            }
+        , ICal.veSummary = Just ICal.Summary
+            { ICal.summaryValue = summary
+            , ICal.summaryAltRep = def
+            , ICal.summaryLanguage = def
+            , ICal.summaryOther = def
+            }
+        , ICal.veDTStart = Just start
+        , ICal.veDTEndDuration = Just $ Left end
+        -- TODO: fill these in with the current time:
+        , ICal.veCreated = Nothing
+        , ICal.veLastMod = Nothing
+
+        , ICal.veDescription =
+            case description of
+                "" ->
+                    Nothing
+
+                _ ->
+                    Just ICal.Description
+                        { ICal.descriptionValue = description
+                        , ICal.descriptionAltRep = def
+                        , ICal.descriptionLanguage = def
+                        , ICal.descriptionOther = def
+                        }
+
+        -- Not used for now:
+        , ICal.veClass = def
+        , ICal.veGeo = def
+        , ICal.veLocation = def
+        , ICal.veOrganizer = def
+        , ICal.vePriority = def
+        , ICal.veSeq = def
+        , ICal.veStatus = def
+        , ICal.veTransp = def
+        , ICal.veUrl = def
+        , ICal.veRecurId = def
+        , ICal.veRRule =
+            case repeats of
+                Nothing -> def
+                Just freq -> S.singleton $ ICal.RRule
+                    { rRuleOther = def
+                    , rRuleValue = ICal.Recur
+                        { recurFreq = freq
+                        , recurUntilCount = def
+                        , recurInterval = 1
+                        , recurBySecond = def
+                        , recurByMinute = def
+                        , recurByHour = def
+                        , recurByDay = def
+                        , recurByMonthDay = def
+                        , recurByYearDay = def
+                        , recurByWeekNo = def
+                        , recurByMonth = def
+                        , recurBySetPos = def
+                        , recurWkSt = ICal.Sunday
+                          -- ^ Does this matter? Not for our current usage,
+                          -- but we should research what it means.
+                        }
+                    }
+        , ICal.veAttach = def
+        , ICal.veAttendee = def
+        , ICal.veCategories = def
+        , ICal.veComment = def
+        , ICal.veContact = def
+        , ICal.veExDate = def
+        , ICal.veRStatus  = def
+        , ICal.veRelated = def
+        , ICal.veResources = def
+        , ICal.veRDate = def
+        , ICal.veAlarms = def
+        , ICal.veOther = def
+        }
+
+encodeTZLabel :: Tz.TZLabel -> LT.Text
+encodeTZLabel =
+    Tz.toTZName
+    >>> LBS.fromStrict
+    >>> LT.decodeUtf8With lenientDecode
--- src/LibMain.hs
+++ src/LibMain.hs
@@ -5,8 +5,6 @@
 module LibMain (main) where
 
 import Data.Default                  (def)
-import Data.Text.Encoding.Error      (lenientDecode)
-import Data.Time.Zones.All           (TZLabel, toTZName)
 import Network.HTTP.Types.Status     (status400, status404)
 import Network.Wai.Parse             (FileInfo(..))
 import SandCal.Config                (cfgDBPath, getConfig)
@@ -18,21 +16,17 @@ import Text.ICalendar.Parser         (parseICalendar)
 import qualified Forms.NewEvent
 import qualified Forms.Settings
 
-import qualified Data.ByteString.Lazy    as LBS
-import qualified Data.Set                as S
-import qualified Data.Text.Lazy          as LT
-import qualified Data.Text.Lazy.Encoding as LT
-import qualified Data.Time               as Time
-import qualified Data.Time.Zones         as Tz
-import qualified Data.Time.Zones.All     as Tz
-import qualified Data.UUID               as UUID
-import qualified Data.UUID.V4            as UUID
-import qualified ICal
+import Data.Time.Zones.All (TZLabel)
+
+import qualified Data.Time           as Time
+import qualified Data.Time.Zones     as Tz
+import qualified Data.Time.Zones.All as Tz
+import qualified Data.UUID.V4        as UUID
 import qualified ICal.Util
 import qualified Route
-import qualified SandCal.DB              as DB
+import qualified SandCal.DB          as DB
 import qualified Sandstorm
-import qualified Util.Time               as UT
+import qualified Util.Time           as UT
 import qualified View
 import qualified View.Import
 
@@ -44,12 +38,6 @@ import Zhp
 
 blaze :: Html -> ActionM ()
 blaze = html . renderHtml
-
-encodeTZLabel :: TZLabel -> LT.Text
-encodeTZLabel =
-    toTZName
-    >>> LBS.fromStrict
-    >>> LT.decodeUtf8With lenientDecode
 
 main :: IO ()
 main = do
@@ -182,125 +170,9 @@ viewSettings db = do
 
 postNewEvent db = do
     utcNow <- liftIO $ Time.getCurrentTime
-    Forms.NewEvent.NewEvent{ summary, description, date, time, repeats } <- Forms.NewEvent.getForm
-
     uuid <- liftIO $ UUID.nextRandom
-
-    let (start, end) = case time of
-            Forms.NewEvent.AllDay ->
-                ( ICal.DTStartDate
-                    { ICal.dtStartOther = def
-                    , ICal.dtStartDateValue = ICal.Date date
-                    }
-                , ICal.DTEndDate
-                    { ICal.dtEndDateValue = ICal.Date date -- TODO/FIXME: should this be exclusive?
-                    , ICal.dtEndOther = def
-                    }
-                )
-            Forms.NewEvent.StartEnd { startTime, endTime, timeZone } ->
-                let floatingStart = Time.LocalTime
-                        { Time.localDay = date
-                        , Time.localTimeOfDay = startTime
-                        }
-                    floatingEnd = floatingStart
-                        { Time.localTimeOfDay = endTime
-                        }
-                    dtStart = ICal.ZonedDateTime
-                        { ICal.dateTimeFloating = floatingStart
-                        , ICal.dateTimeZone = encodeTZLabel timeZone
-                        }
-                    dtEnd = dtStart { ICal.dateTimeFloating = floatingEnd }
-                in
-                ( ICal.DTStartDateTime
-                    { ICal.dtStartOther = def
-                    , ICal.dtStartDateTimeValue = dtStart
-                    }
-                , ICal.DTEndDateTime
-                    { ICal.dtEndDateTimeValue = dtEnd
-                    , ICal.dtEndOther = def
-                    }
-                )
-        vEvent = ICal.VEvent
-            { ICal.veUID = ICal.UID
-                { ICal.uidValue = LT.fromStrict $ UUID.toText uuid
-                , ICal.uidOther = def
-                }
-            , ICal.veDTStamp = ICal.DTStamp
-                { ICal.dtStampValue = utcNow
-                , ICal.dtStampOther = def
-                }
-            , ICal.veSummary = Just ICal.Summary
-                { ICal.summaryValue = summary
-                , ICal.summaryAltRep = def
-                , ICal.summaryLanguage = def
-                , ICal.summaryOther = def
-                }
-            , ICal.veDTStart = Just start
-            , ICal.veDTEndDuration = Just $ Left end
-            -- TODO: fill these in with the current time:
-            , ICal.veCreated = Nothing
-            , ICal.veLastMod = Nothing
-
-            , ICal.veDescription =
-                case description of
-                    "" ->
-                        Nothing
-
-                    _ ->
-                        Just ICal.Description
-                            { ICal.descriptionValue = description
-                            , ICal.descriptionAltRep = def
-                            , ICal.descriptionLanguage = def
-                            , ICal.descriptionOther = def
-                            }
-
-            -- Not used for now:
-            , ICal.veClass = def
-            , ICal.veGeo = def
-            , ICal.veLocation = def
-            , ICal.veOrganizer = def
-            , ICal.vePriority = def
-            , ICal.veSeq = def
-            , ICal.veStatus = def
-            , ICal.veTransp = def
-            , ICal.veUrl = def
-            , ICal.veRecurId = def
-            , ICal.veRRule =
-                case repeats of
-                    Nothing -> def
-                    Just freq -> S.singleton $ ICal.RRule
-                        { rRuleOther = def
-                        , rRuleValue = ICal.Recur
-                            { recurFreq = freq
-                            , recurUntilCount = def
-                            , recurInterval = 1
-                            , recurBySecond = def
-                            , recurByMinute = def
-                            , recurByHour = def
-                            , recurByDay = def
-                            , recurByMonthDay = def
-                            , recurByYearDay = def
-                            , recurByWeekNo = def
-                            , recurByMonth = def
-                            , recurBySetPos = def
-                            , recurWkSt = ICal.Sunday
-                              -- ^ Does this matter? Not for our current usage,
-                              -- but we should research what it means.
-                            }
-                        }
-            , ICal.veAttach = def
-            , ICal.veAttendee = def
-            , ICal.veCategories = def
-            , ICal.veComment = def
-            , ICal.veContact = def
-            , ICal.veExDate = def
-            , ICal.veRStatus  = def
-            , ICal.veRelated = def
-            , ICal.veResources = def
-            , ICal.veRDate = def
-            , ICal.veAlarms = def
-            , ICal.veOther = def
-            }
+    form <- Forms.NewEvent.getForm
+    let vEvent = Forms.NewEvent.toVEvent utcNow uuid form
     evId <- DB.runQuery db $ DB.addEvent vEvent
     Route.redirectGet $ Route.Event (DB.unEventID evId) Nothing
 
