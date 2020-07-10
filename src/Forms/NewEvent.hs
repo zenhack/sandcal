21575a3eb7cf549a6dd21fed1a9f61d5f21338a8
Ian Denhardt <ian@zenhack.net>
2020-07-10 17:24:18 -0400

    Write a helper to patch an existing VEvent.
    Not wired up to anything yet.

--- src/Forms/NewEvent.hs
+++ src/Forms/NewEvent.hs
@@ -1,3 +1,7 @@
+-- | New event/event modification forms.
+--
+-- TODO: rename this; it used to just be for creating new events, but we are
+-- also using this to edit existing ones now.
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns        #-}
 module Forms.NewEvent
@@ -5,6 +9,7 @@ module Forms.NewEvent
     , NewEventTime(..)
     , getForm
     , toVEvent
+    , patchVEvent
     ) where
 
 import Zhp
@@ -129,6 +134,73 @@ getStartEnd form = case time form of
             }
         )
 
+-- | Patch an existing vevent based on a modification time and event form.
+patchVEvent :: Time.UTCTime -> NewEvent -> ICal.VEvent -> ICal.VEvent
+patchVEvent utcNow form old =
+    let (start, end) = getStartEnd form in
+    old
+        { ICal.veSummary = patchTextField (ICal.veSummary old) (summary form)
+        , ICal.veDescription = patchTextField (ICal.veDescription old) (description form)
+        , ICal.veLocation = patchTextField (ICal.veLocation old) (location form)
+        , ICal.veDTStart = Just start
+        , ICal.veDTEndDuration = Just $ Left end
+        , ICal.veLastMod = Just ICal.LastModified
+            { lastModifiedValue = utcNow
+            , lastModifiedOther = def
+            }
+        , ICal.veRRule = makeRRule (repeats form)
+        }
+
+patchTextField :: FromTextField a => Maybe a -> LT.Text -> Maybe a
+patchTextField old newText =
+    case old of
+        Nothing   -> fromTextField newText
+        Just old' -> Just $ setTextField newText old'
+
+class FromTextField a where
+    fromTextField' :: LT.Text -> a
+    setTextField :: LT.Text -> a -> a
+
+fromTextField :: FromTextField a => LT.Text -> Maybe a
+fromTextField txt
+    | LT.null txt = Nothing
+    | otherwise = Just (fromTextField' txt)
+
+
+instance FromTextField ICal.Summary where
+    fromTextField' txt = ICal.Summary
+        { summaryValue = txt
+        , summaryAltRep = def
+        , summaryLanguage = def
+        , summaryOther = def
+        }
+    setTextField txt old =
+        old { ICal.summaryValue = txt }
+
+instance FromTextField ICal.Location where
+    fromTextField' txt = ICal.Location
+        { locationValue = txt
+        , locationAltRep = def
+        , locationLanguage = def
+        , locationOther = def
+        }
+    setTextField txt old =
+        old { ICal.locationValue = txt }
+
+instance FromTextField ICal.Description where
+    fromTextField' txt = ICal.Description
+        { descriptionValue = txt
+        , descriptionAltRep = def
+        , descriptionLanguage = def
+        , descriptionOther = def
+        }
+    setTextField txt old =
+        old { ICal.descriptionValue = txt }
+
+-- | Convert user-supplied event form into an icalendar event.
+--
+-- Also accepts as arguments the current time, and a uuid to use for the events'
+-- unique id.
 toVEvent :: Time.UTCTime -> UUID.UUID -> NewEvent -> ICal.VEvent
 toVEvent utcNow uuid form =
     let (start, end) = getStartEnd form in
@@ -141,12 +213,7 @@ toVEvent utcNow uuid form =
             { dtStampValue = utcNow
             , dtStampOther = def
             }
-        , veSummary = Just ICal.Summary
-            { summaryValue = summary form
-            , summaryAltRep = def
-            , summaryLanguage = def
-            , summaryOther = def
-            }
+        , veSummary = fromTextField (summary form)
         , veDTStart = Just start
         , veDTEndDuration = Just $ Left end
         , veCreated = Just ICal.Created
@@ -157,49 +224,9 @@ toVEvent utcNow uuid form =
             { lastModifiedValue = utcNow
             , lastModifiedOther = def
             }
-        , veDescription =
-            if LT.null $ description form then
-                Nothing
-            else
-                Just ICal.Description
-                    { descriptionValue = description form
-                    , descriptionAltRep = def
-                    , descriptionLanguage = def
-                    , descriptionOther = def
-                    }
-        , veRRule =
-            case repeats form of
-                Nothing -> def
-                Just freq -> S.singleton $ ICal.RRule
-                    { rRuleOther = def
-                    , rRuleValue = ICal.Recur
-                        { recurFreq = freq
-                        , recurUntilCount = def
-                        , recurInterval = 1
-                        , recurBySecond = def
-                        , recurByMinute = def
-                        , recurByHour = def
-                        , recurByDay = def
-                        , recurByMonthDay = def
-                        , recurByYearDay = def
-                        , recurByWeekNo = def
-                        , recurByMonth = def
-                        , recurBySetPos = def
-                        , recurWkSt = ICal.Sunday
-                          -- ^ Does this matter? Not for our current usage,
-                          -- but we should research what it means.
-                        }
-                    }
-        , veLocation =
-            if LT.null $ location form then
-                Nothing
-            else
-                Just ICal.Location
-                    { locationValue = location form
-                    , locationAltRep = def
-                    , locationLanguage = def
-                    , locationOther = def
-                    }
+        , veDescription = fromTextField (description form)
+        , veRRule = makeRRule (repeats form)
+        , veLocation = fromTextField (location form)
 
         -- Not used for now:
         , veClass = def
@@ -223,6 +250,30 @@ toVEvent utcNow uuid form =
         , veRDate = def
         , veAlarms = def
         , veOther = def
+        }
+
+makeRRule :: Maybe ICal.Frequency -> S.Set ICal.RRule
+makeRRule Nothing = def
+makeRRule (Just freq) =
+    S.singleton $ ICal.RRule
+        { rRuleOther = def
+        , rRuleValue = ICal.Recur
+            { recurFreq = freq
+            , recurUntilCount = def
+            , recurInterval = 1
+            , recurBySecond = def
+            , recurByMinute = def
+            , recurByHour = def
+            , recurByDay = def
+            , recurByMonthDay = def
+            , recurByYearDay = def
+            , recurByWeekNo = def
+            , recurByMonth = def
+            , recurBySetPos = def
+            , recurWkSt = ICal.Sunday
+              -- ^ Does this matter? Not for our current usage,
+              -- but we should research what it means.
+            }
         }
 
 encodeTZLabel :: Tz.TZLabel -> LT.Text
