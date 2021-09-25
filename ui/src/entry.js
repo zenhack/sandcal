4f35e3695a6b2033b5342f71973461bd96e3b16d
Ian Denhardt <ian@zenhack.net>
2021-09-25 11:47:10 -0400

    Remove some logging statements.

--- ui/src/entry.js
+++ ui/src/entry.js
@@ -19,8 +19,5 @@ document.addEventListener('DOMContentLoaded', function() {
   var browserTz = new Intl.DateTimeFormat().resolvedOptions().timeZone;
   var elem = document.getElementById('bs-form');
   var template = elem.attributes['data-sandcal-template'].nodeValue;
-  console.log("template:");
-  console.log(JSON.parse(template));
-  console.log("browserTz = ", browserTz);
   main(template, browserTz)(elem);
 })
