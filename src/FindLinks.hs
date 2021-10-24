9c7d196d94ea036ccb3297b0cc5a1fbab9193bce
Ian Denhardt <ian@zenhack.net>
2021-10-24 16:41:02 -0400

    FindLinks: use Text builders internally.
    Should improve performance, though I haven't measured.

--- src/FindLinks.hs
+++ src/FindLinks.hs
@@ -6,14 +6,15 @@ module FindLinks
 
 import Zhp hiding (many)
 
-import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy         as LT
+import qualified Data.Text.Lazy.Builder as TB
 
 import Data.List            (intersperse)
 import Data.Void            (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-type NodeParser = NodeType -> LT.Text -> Parser Node
+type NodeParser = NodeType -> TB.Builder -> Parser Node
 type Parser = Parsec Void LT.Text
 
 data Renderer a = Renderer
@@ -41,21 +42,24 @@ data NodeType
 
 data Node = Node
     { nodeType  :: !NodeType
-    , nodeValue :: LT.Text
+    , nodeValue :: TB.Builder
     }
-    deriving(Show, Read, Eq)
+    deriving(Show, Eq)
 
 renderNode :: Renderer a -> Node -> a
 renderNode r node =
     let v = nodeValue node in
     case nodeType node of
-        Text   -> renderText r v
-        HTTP   -> renderLink r v v
-        WWW    -> renderLink r ("https://" <> v) v
-        Mailto -> renderLink r v v
-        Email  -> renderLink r ("mailto:" <> v) v
+        Text   -> text v
+        HTTP   -> link v v
+        WWW    -> link ("https://" <> v) v
+        Mailto -> link v v
+        Email  -> link ("mailto:" <> v) v
+  where
+    link l txt = renderLink r (TB.toLazyText l) (TB.toLazyText txt)
+    text txt =  renderText r (TB.toLazyText txt)
 
-breakPoint :: NodeType -> LT.Text -> NodeParser -> Parser Node
+breakPoint :: NodeType -> TB.Builder -> NodeParser -> Parser Node
 breakPoint typ prefix parser =
     try (parser typ prefix) <|> pure (Node typ prefix)
 
@@ -74,15 +78,18 @@ isUnreserved c =
     || isSafe c
     || isExtra c
 
-pHex :: Parser LT.Text
-pHex = LT.pack . pure <$> satisfy isHexDigit
+pHex :: Parser TB.Builder
+pHex = TB.singleton <$> satisfy isHexDigit
 
-takeWhilePOrEscaped :: String -> (Char -> Bool) -> Parser LT.Text
+takeWhilePOrEscaped :: String -> (Char -> Bool) -> Parser TB.Builder
 takeWhilePOrEscaped name p =
-    mconcat <$> many (takeWhile1P (Just name) p <|> pEscaped)
+    mconcat <$> many (choice
+        [ TB.fromLazyText <$> takeWhile1P (Just name) p
+        , pEscaped
+        ])
 
-pEscaped :: Parser LT.Text
-pEscaped = try $ chain [ string "%", pHex, pHex ]
+pEscaped :: Parser TB.Builder
+pEscaped = try $ chain [ TB.singleton <$> char '%', pHex, pHex ]
 
 isSegChar, isSearchChar, isFragmentChar :: Char -> Bool
 isSegChar c =
@@ -93,7 +100,7 @@ isFragmentChar = isSegChar
 
 pHttpUrl :: Parser Node
 pHttpUrl = try $ do
-    prefix <- chain
+    prefix <- chain $ map (fmap TB.fromLazyText)
         [ string "http"
         , fromMaybe "" <$> optional (string "s")
         , string "://"
@@ -102,7 +109,7 @@ pHttpUrl = try $ do
 
 pWWWUrl :: Parser Node
 pWWWUrl = try $ do
-    prefix <- string "www."
+    prefix <- TB.fromLazyText <$> string "www."
     pHttpUrlTail WWW prefix
 
 pHttpUrlTail :: NodeParser
@@ -115,19 +122,19 @@ pHostPortTail typ prefix = do
     port <- fromMaybe "" <$> optional pPort
     breakPoint typ (prefix <> port) pPostPortTail
 
-pHost :: Parser LT.Text
+pHost :: Parser TB.Builder
 pHost = do
-    prefix <- chain [ pHostPart, string "." ]
+    prefix <- chain [ pHostPart, TB.singleton <$> char '.' ]
     parts <- pHostPart `sepBy1` string "."
     pure $ mconcat $ prefix : intersperse "." parts
 
-pHostPart :: Parser LT.Text
-pHostPart = takeWhileP
+pHostPart :: Parser TB.Builder
+pHostPart = TB.fromLazyText <$> takeWhileP
     (Just "HostPart")
     (\c -> isAlphaNum c || c == '-')
 
-pPort :: Parser LT.Text
-pPort = try $ chain
+pPort :: Parser TB.Builder
+pPort = try $ chain $ map (fmap TB.fromLazyText)
     [ string ":"
     , takeWhileP (Just "digit") isDigit
     ]
@@ -148,7 +155,7 @@ pPath typ prefix = do
         , char '#' *> pFragment t p
         ]
 
-pPathSegment :: Parser LT.Text
+pPathSegment :: Parser TB.Builder
 pPathSegment = takeWhilePOrEscaped "Path segment" isSegChar
 
 pQuery :: NodeParser
@@ -171,9 +178,7 @@ pNode = choice
     ]
 
 pChar :: Parser Node
-pChar = do
-    c <- anySingle
-    pure $ Node Text $ LT.pack [c]
+pChar = Node Text . TB.singleton <$> anySingle
 
 toNodes :: LT.Text -> [Node]
 toNodes txt =
